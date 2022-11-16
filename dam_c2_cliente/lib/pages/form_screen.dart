038dba617558e702cda9109d3b7e1ff015269e0a
Javier Rojas <javier4le.dev@gmail.com>
2022-11-15 23:53:07 -0300

    add form fields required validacion

--- dam_c2_cliente/lib/pages/form_screen.dart
+++ dam_c2_cliente/lib/pages/form_screen.dart
@@ -20,6 +20,8 @@ class _FormScreenState extends State<FormScreen> {
   final DamC2ApiProvider provider = new DamC2ApiProvider();
   String autos = '';
 
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,119 +35,136 @@ class _FormScreenState extends State<FormScreen> {
         centerTitle: true,
       ),
       body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Agregar un nuevo auto',
-                style: TextStyle(fontSize: 30),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              // vin
-              TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese el VIN';
-                  }
-                  return null;
-                },
-                controller: vin,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'VIN',
-                  hintText: 'Ingrese el VIN',
+        child: Form(
+          key: formKey,
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Agregar un nuevo auto',
+                  style: TextStyle(fontSize: 30),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese la marca';
-                  }
-                  return null;
-                },
-                controller: marca,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Marca',
-                  hintText: 'Ingrese la marca del auto',
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese el modelo';
-                  }
-                  return null;
-                },
-                controller: modelo,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Modelo',
-                  hintText: 'Ingrese el modelo del auto',
+                // vin
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingrese el VIN';
+                    }
+                    return null;
+                  },
+                  controller: vin,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'VIN',
+                    hintText: 'Ingrese el VIN',
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese el año';
-                  }
-                  return null;
-                },
-                controller: annio,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Año',
-                  hintText: 'Ingrese el año del auto',
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              // precio
-              TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese el precio';
-                  }
-                  return null;
-                },
-                controller: precio,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Precio',
-                  hintText: 'Ingrese el precio del auto',
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingrese la marca';
+                    }
+                    return null;
+                  },
+                  controller: marca,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Marca',
+                    hintText: 'Ingrese la marca del auto',
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // agregar el auto con provider y navegar a la pantalla de autos
-                  provider.addAutos(vin.text, marca.text, modelo.text,
-                      int.parse(annio.text), int.parse(precio.text));
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingrese el modelo';
+                    }
+                    return null;
+                  },
+                  controller: modelo,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Modelo',
+                    hintText: 'Ingrese el modelo del auto',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingrese el año';
+                    }
+                    return null;
+                  },
+                  controller: annio,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Año',
+                    hintText: 'Ingrese el año del auto',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                // precio
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingrese el precio';
+                    }
+                    return null;
+                  },
+                  controller: precio,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Precio',
+                    hintText: 'Ingrese el precio del auto',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      provider
+                          .addAutos(vin.text, marca.text, modelo.text,
+                              int.parse(annio.text), int.parse(precio.text))
+                          .then((value) {
+                        if (value == 'OK') {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Home(),
+                            ),
+                          );
+                        }
+                      });
+                    }
+                    // provider.addAutos(vin.text, marca.text, modelo.text,
+                    //     int.parse(annio.text), int.parse(precio.text));
 
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => Home()));
-                },
-                child: const Text('Agregar'),
-              ),
-            ],
+                    // Navigator.push(
+                    //     context, MaterialPageRoute(builder: (context) => Home()));
+                  },
+                  child: const Text('Agregar'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
